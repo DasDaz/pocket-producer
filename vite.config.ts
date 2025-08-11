90306dd206a302a0c06edf6a1eb1f0651b604c73
Daniel <DasDaz@users.noreply.github.com>
2025-08-11 23:42:34 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 // If you use the default GitHub Pages URL, keep base as '/pocket-producer/'.
 // If you use a custom domain, change base to '/'.
 export default defineConfig({
-  base: '/pocket-producer/',
+  base: '/',
   plugins: [react()]
 })
